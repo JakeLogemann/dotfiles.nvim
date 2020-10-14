49847ee97a90c2771522c2bd2ca27589277dbd94
Jake Logemann <jake@lgmn.io>
2020-10-14 01:12:02 +0000

    add logging, json support& nvim_rocks starter.

--- lua/vimrc/init.lua
+++ lua/vimrc/init.lua
@@ -1,5 +1,21 @@
 local M = { plugins = {} }
-require('vimrc/globals')
+
+vim.env.GIT_ASKPASS = "true"
+
+vim.api.nvim_set_var('libmodalTimeouts', true) -- define modal timeouts; avoid a bug in libmodal!
+
+_G['dbg'] = function(...) print(unpack(vim.tbl_map(vim.inspect, {...}))) end
+_G['log'] = require('vimrc/log')
+_G['json'] = require('vimrc/json')
+_G['vimp'] = require('vimp')
+_G['libmodal'] = require('libmodal')
+_G['plenary'] = require('plenary')
+_G['bufferline'] = require('bufferline')
+_G['devicons'] = require('nvim-web-devicons')
+_G['colorbuddy'] = require('colorbuddy')
+
+devicons.setup()
+
 require('vimrc/options')
 require('vimrc/mappings')
 require('vimrc/packages')
--- lua/vimrc/plugins/colorbuddy.lua
+++ lua/vimrc/plugins/colorbuddy.lua
@@ -1,13 +1,12 @@
- 
 -- luacheck: Globals Color, c, Group, g, s
-require("colorbuddy").setup()
+local Color, colors, Group, groups, styles = require('colorbuddy').setup()
 
 Color.new('white',     '#f2e5bc')
-Color.new('red',       '#E06C75')
+Color.new('red',       '#cc6666')
 Color.new('pink',      '#fef601')
-Color.new('green',     '#97C379')
+Color.new('green',     '#99cc99')
 Color.new('yellow',    '#f8fe7a')
-Color.new('blue',      '#61AFEF')
+Color.new('blue',      '#81a2be')
 Color.new('aqua',      '#8ec07c')
 Color.new('cyan',      '#8abeb7')
 Color.new('purple',    '#8e6fbd')
@@ -17,3 +16,9 @@ Color.new('brown',     '#a3685a')
 
 Color.new('seagreen',  '#698b69')
 Color.new('turquoise', '#698b69')
+
+-- If you want multiple styles, just add them!
+Group.new('italicBoldFunction', colors.green, groups.Function, styles.bold + styles.italic)
+
+-- If you want the same style as a different group, but without a style: just subtract it!
+Group.new('boldFunction', colors.yellow, colors.background, groups.italicBoldFunction - styles.italic)
